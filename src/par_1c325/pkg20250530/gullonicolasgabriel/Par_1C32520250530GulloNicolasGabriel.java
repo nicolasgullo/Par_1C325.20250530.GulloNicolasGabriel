a3a44b5678d9a131910bc1d729dd31594b8c8506
nicolasgullo <nicolas_gullo@hotmail.com>
2025-05-24 18:02:25 -0300

    prueba Parcial

---
+++ src/par_1c325/pkg20250530/gullonicolasgabriel/Par_1C32520250530GulloNicolasGabriel.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package par_1c325.pkg20250530.gullonicolasgabriel;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gullito
+ */
+public class Par_1C32520250530GulloNicolasGabriel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int opcion;
+        do {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            opcion = sc.nextInt();
+            sc.nextLine();
+            
+            switch (opcion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }       
+        }while (opcion != 0);
+    }
+    
+}
